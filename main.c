ae6548a4b403c5b5a310bbc21c193182a136c740
loichu <loichumbert5450@gmail.com>
2022-01-25 17:01:30 +0100

    Initial sort working

--- main.c
+++ main.c
@@ -34,31 +34,51 @@ void	raise_error(int errcode)
 	exit(errcode);
 }
 
+void	incr_order(t_list *node)
+{
+	if (!node)
+		return ;
+	t_node	*c;
+	t_node	*n;
+	n = (t_node *)node->next->content;
+	c = (t_node *)node->content;
+	n->ord = c->ord + 1;
+	incr_order(node->next);
+}
+
+void	ins_node(t_list **lst, t_list *new, t_list *prev, t_list *curr)
+{
+		
+	if (!prev)
+		*lst = new;
+	else
+		prev->next = new;
+	new->next = curr;
+	//incr_order(curr);
+}
+
 void	lstadd_sort(t_list **lst, t_node *node)
 {
-	t_list	*curr;	
+	t_node	*curr_node;
+	t_list	*curr;
 	t_list	*prev;
 	t_list	*new;
+	int		i;
 
 	curr = *lst;
 	new = ft_lstnew(node);
 	prev = NULL;
+	i = 0;
 	while (curr)
 	{
-		if (node->val == curr->content->val)
+		curr_node = (t_node *)curr->content;
+		curr_node->ord = i++;
+		if (node->val == curr_node->val)
 			raise_error(1);
-		else if (node->val < curr->content->val)
-		{
-			if (prev)
-				prev->next = new;
-			new->next = curr;
-			curr = new;
-			if (!prev)
-				*lst = curr;	
-			return ;
-		}
-		prev = lst;
-		lst = lst->next;
+		else if (node->val < curr_node->val)
+			return ins_node(lst, new, prev, curr);
+		prev = curr;
+		curr = curr->next;
 	}
 	prev->next = new;
 }
@@ -68,6 +88,10 @@ void	pval(void *node)
 	t_node	*n;
 	n = (t_node *)node;
 	ft_putnbr_fd(n->val, 1);
+	ft_putchar_fd('\t', 1);
+	ft_putnbr_fd(n->ini, 1);
+	ft_putchar_fd('\t', 1);
+	ft_putnbr_fd(n->ord, 1);
 	ft_putchar_fd('\n', 1);
 }
 
@@ -79,7 +103,8 @@ int	main(int argc, char **argv)
 	i = 1;
 	in = ft_lstnew(create_node(ft_atoi(argv[i]), i - 1));
 	while(++i < argc)
-		ft_lstadd_back(&in, ft_lstnew(create_node(ft_atoi(argv[i]), i - 1)));
+		lstadd_sort(&in, create_node(ft_atoi(argv[i]), i - 1));
+	ft_putstr_fd("Value\tInit\tOrder\n", 1);
 	ft_lstiter(in, *pval);
 	return (0);
 }
