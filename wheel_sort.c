7fac33e471e0055df61808a08d50de746fe0f0dc
Loic Humbert <loichumbert5450@gmail.com>
2022-03-28 15:13:53 +0200

    output seems ok

--- wheel_sort.c
+++ wheel_sort.c
@@ -10,27 +10,34 @@ void	make_moves(t_stacks **stacks, t_node *best_node)
 		exec_rotates(stacks, 'b', best_node->move_b, false);
 }
 
+int	get_move_a(int node_val, t_node *stack_a, int a_size)
+{
+	t_node	*prev;
+	int 	move;
+
+	move = 0;
+	prev = node_last(stack_a);
+	while (!(node_val < stack_a->val && node_val > prev->val))
+	{
+		move++;
+		prev = stack_a;
+		stack_a = stack_a->next;
+	}
+	if (move > a_size / 2)
+		move = (a_size - move) * -1;
+	return (move);
+}
+
 void	get_scores(t_stacks *stacks)
 {
 	int 	i;
 	t_node 	*node;
-	t_node 	*stack_a;
-	t_node	*prev;
 
 	node = stacks->b;
 	i = -1;
 	while (++i < stacks->size_b)
 	{
-		stack_a = stacks->a;
-		prev = node_last(stacks->a);
-		node->move_a = 0;
-		while (!(node->val < stack_a->val && node->val > prev->val))
-		{
-			//printf("val: %i, prev: %i, curr: %i\n", node->val, prev->val, stack_a->val);
-			node->move_a++;
-			prev = stack_a;
-			stack_a = stack_a->next;
-		}
+		node->move_a = get_move_a(node->val, stacks->a, stacks->size_a);
 		//printf("got move a\n");
 		node->move_b = move_to_top(i, stacks->size_b);
 		node->score = calc_score(node);
@@ -39,12 +46,39 @@ void	get_scores(t_stacks *stacks)
 	}
 }
 
+void    print_stacks(t_node *stack_b, t_node *stack_a)
+{
+	printf("B\tMB\tS\tMA\tA\n");
+	printf("====================================\n");
+	while (stack_a && stack_b)
+	{
+		printf("%i\t%i\t%i\t%i\t%i\n", stack_b->val, stack_b->move_b, stack_b->score, stack_b->move_a, stack_a->val);
+		stack_b = stack_b->next;
+		stack_a = stack_a->next;
+	}
+	while (stack_b)
+	{
+		printf("%i\t%i\t%i\t%i\n", stack_b->val, stack_b->move_b, stack_b->score, stack_b->move_a);
+		stack_b = stack_b->next;
+	}
+	while (stack_a)
+	{
+		printf("\t\t\t\t%i\n", stack_a->val);
+		stack_a = stack_a->next;
+	}
+}
+
 void	sort(t_stacks **stacks)
 {
+	char    *press_enter;
+
+	press_enter = "";
 	while ((*stacks)->size_b > 0)
 	{
 		//printf("loop\n");
 		get_scores(*stacks);
+		print_stacks((t_node *)(*stacks)->b, (t_node *)(*stacks)->a);
+		read(1, press_enter, 1);
 		//printf("got scores\n");
 		make_moves(stacks, get_best_node((*stacks)->b));
 		p('a', stacks);
